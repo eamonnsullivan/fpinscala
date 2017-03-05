2613a19cf25df8382915d6b5dc923762f48107b7
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2017-03-05 14:47:28 +0000

    chapter 2 exercises.

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,21 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(count: Int, cur: Int, prev: Int): Int = 
+      if (count == n)
+        cur
+      else 
+        go(count + 1, cur + prev, cur)
+
+    if (n <= 0)
+      0
+    else if (n == 1)
+      1
+    else
+      go(2, 1, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +154,22 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def isOrdered(elem: A, rest: Array[A]): Boolean = {
+      if (rest.isEmpty)
+        true
+      else if (!gt(elem, rest.head))
+        false
+      else
+        isOrdered(rest.head, rest.tail)
+    }
+
+    if (as.isEmpty || as.length == 1)
+      false
+    else
+      isOrdered(as.head, as.tail)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -153,13 +182,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => f(a, _)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -174,5 +203,5 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a: A) => f(g(a))
 }
